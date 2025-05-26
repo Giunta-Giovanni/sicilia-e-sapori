c86ad2fe4c9b3e2455c80b5e236d2e26170fb322
GiuntaGiovanni <jhonniest741@gmail.com>
2025-05-26 12:40:13 +0200

    feat: refactor FoodsSeeder to improve readability by nesting food sections

--- database/seeders/FoodsSeeder.php
+++ database/seeders/FoodsSeeder.php
@@ -14,24 +14,26 @@ class FoodsSeeder extends Seeder
     public function run(): void
     {
         $foods = config('data.foods');
-
         // creiamo un ciclo per ogni cibo che abbiamo
-        foreach ($foods as $food) {
-            // creiamo l'istanza Food
-            $newFood = new Food();
+        foreach ($foods as $foodsections) {
+            foreach ($foodsections as $food) {
 
-            $newFood->id_food_category = $food['id_food_category'];
-            $newFood->name_it = $food['name_it'];
-            $newFood->name_eng = $food['name_eng'];
-            $newFood->description_it = $food['description_it'];
-            $newFood->description_eng = $food['description_eng'];
-            $newFood->primary_price = $food['primary_price'];
-            $newFood->secondary_price = $food['secondary_price'];
-            $newFood->is_spicy = $food['is_spicy'];
-            $newFood->is_vegetarian = $food['is_vegetarian'];
+                $newFood = new Food();
 
-            // salviamo l'istanza
-            $newFood->save();
+                $newFood->id_food_category = $food['id_food_category'];
+                // creiamo l'istanza Food
+                $newFood->name_it = $food['name_it'];
+                $newFood->name_eng = $food['name_eng'];
+                $newFood->description_it = $food['description_it'];
+                $newFood->description_eng = $food['description_eng'];
+                $newFood->primary_price = $food['primary_price'] === null ? 0 : $food['primary_price'];
+                $newFood->secondary_price = $food['secondary_price'];
+                $newFood->is_spicy = $food['is_spicy'];
+                $newFood->is_vegetarian = $food['is_vegetarian'];
+
+                // salviamo l'istanza
+                $newFood->save();
+            }
         }
     }
 }
