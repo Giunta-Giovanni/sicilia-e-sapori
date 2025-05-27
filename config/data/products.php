9d5ac05bd2aade837ba2881ac75cca453aca65e2
GiuntaGiovanni <jhonniest741@gmail.com>
2025-05-27 03:17:22 +0200

    feat: refactor product data generation for improved clarity and structure

--- config/data/products.php
+++ config/data/products.php
@@ -1,9 +1,28 @@
 <?php
-return [
-    [1, null],
-    [2, null],
-    [3, null],
-    [null, 1],
-    [null, 2],
-    [null, 3]
-];
+
+$foods = config('data.foods');
+
+$foodmax = count($foods[0]);
+
+
+$beverages = config('data.beverages');
+
+$beveragemax = count($beverages[0]);
+
+
+// creare un array di valori 0 e 1
+$product = [];
+// ciclare tot di volte in base al numero di cibi
+// pushare il risultato dentro all'array
+for ($i = 0; $i < $foodmax; $i++) {
+    $items = [$i + 1, null];
+    array_push($product, $items);
+};
+for ($i = 0; $i < $beveragemax; $i++) {
+    // $index = $i + 1;
+    $items = [null, $i + 1];
+    array_push($product, $items);
+};
+// ciclare tot di volte in base al numero di bevande
+
+return $product;
